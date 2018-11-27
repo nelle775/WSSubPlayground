37617b5d46ceb4a58315920b0e554873802874fa
unknown <bootcamp.studios@gmail.com>
2018-11-27 08:20:24 +0100

    map stuff

---
+++ WebSocketsTest/Models/Protocol/Update.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace WebSocketsTest.Models.Protocol
+{
+    public class Update : IMessage
+    {
+        public MessageType Type { get { return MessageType.Update; } }
+        public string SubscriptionId { get; set; }
+        public string MessagePrimaryKey {
+            get {
+                if (Model == null) return null;
+                return Model.PrimaryKey;
+            }
+        }
+        public IRealtimeModel Model { get; set; }
+
+        public void ParseMessage(JsonReader reader)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string ToJson()
+        {
+            MemoryStream ms = new MemoryStream();
+            StreamWriter sw = new StreamWriter(ms, new UTF8Encoding(false));
+            JsonTextWriter writer = new JsonTextWriter(sw);
+
+            writer.WriteStartObject();
+            writer.WritePropertyName("msg");
+            writer.WriteValue(Type.ToString());
+            writer.WritePropertyName("subscriptionId");
+            writer.WriteValue(SubscriptionId);
+            writer.WritePropertyName("modelPrimaryKey");
+            writer.WriteValue(MessagePrimaryKey);
+            writer.WritePropertyName("model");
+            
+
+            JObject jObject = JObject.FromObject(Model);
+            jObject.WriteTo(writer);
+
+            writer.WriteEndObject();
+
+            writer.Close();
+
+            string json = Encoding.UTF8.GetString(ms.ToArray());
+            ms.Dispose();
+            return json;
+        }
+    }
+}
---
+++ WebSocketsTest/Models/RealtimeMarker.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebSocketsTest.Models
+{
+    public class RealtimeMarker : IRealtimeModel
+    {
+        public string PrimaryKey {
+            get { return Id; }
+        }
+
+        public string Id { get; set; }
+        public string Label { get; set; }
+        public double Lat { get; set; }
+        public double Lon { get; set; }
+
+    }
+}
